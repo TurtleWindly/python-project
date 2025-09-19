06d5b89f6bb2971d288e4279b05dda7ac4df422a
MinhWin <quachquangminh2006@gmail.com>
2025-09-19 16:57:59 +0700

    pong kivy turtorial complete

--- pong/main.py
+++ pong/main.py
@@ -1,11 +1,20 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
+from kivy.uix.label import Label
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
+
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            speedup = 1.1
+            offset = 0.02 * Vector(0, ball.center_y - self.center_y)
+            ball.velocity = speedup * (offset - ball.velocity)
 
 
 class PongBall(Widget):
@@ -19,13 +28,44 @@ class PongBall(Widget):
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
+    player1: PongPaddle = ObjectProperty(None)
+    player2: PongPaddle = ObjectProperty(None)
+    max_score: int = 10
+
+    def check_winning_condition(self):
+        if self.player1.score >= self.max_score or self.player2.score >= self.max_score:
+            self.show_winner()
+
+    def show_winner(self):
+        Clock.unschedule(self.update)
+        win_label = Label(
+            text = "hahah",
+            font_size = 50,
+            center = self.center
+        )
+        self.add_widget(win_label)
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.player2.center_y = touch.y
 
     def serve_ball(self):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = Vector(10, 0).rotate(randint(0, 360))
 
     def update(self, dt):
         self.ball.move()
+
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+        self.check_winning_condition()
+
+        if self.ball.x < self.x:
+            self.player2.score += 1
+        if self.ball.right > self.width:
+            self.player1.score += 1
 
         # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
@@ -44,5 +84,5 @@ class PongApp(App):
         return game
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     PongApp().run()
