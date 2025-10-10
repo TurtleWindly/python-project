1aba0c1d6aa00332a1d287a2bd9e7610af45102b
MinhWin <quachquangminh2006@gmail.com>
2025-10-10 22:31:43 +0700

    register with input form complete

--- quiz box/app/views/registerscreen/registerscreen.py
+++ quiz box/app/views/registerscreen/registerscreen.py
@@ -1,11 +1,20 @@
-import json
-import random
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
+from kivy.properties import ObjectProperty
 
 
 class RegisterScreen(Screen):
     name = "quiz_register_screen"
+
+    # expose the id widgets to python if you want to access them via attributes
+    name_text = ObjectProperty(None)
+    unit_text = ObjectProperty(None)
+
+    # prefer a clearer method name, but reg is fine
+    def register_user(self):
+        name = self.name_text.text_input.text
+        unit = self.unit_text.text_input.text
+
+        if name and unit:
+            user_data = {"name": name, "unit": unit, "score": 0}
+            # do something with user_data, e.g. save or switch screen
+        print(name, unit)
