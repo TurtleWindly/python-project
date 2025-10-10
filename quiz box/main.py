3a23513fa815a6530f559c812978a1ed2e8b0dfd
MinhWin <quachquangminh2006@gmail.com>
2025-10-10 19:43:31 +0700

    added registerscreen

--- quiz box/main.py
+++ quiz box/main.py
@@ -4,6 +4,7 @@ from kivy.app import App
 
 from app.views.homescreen.homescreen import HomeScreen
 from app.views.quizscreen.quizscreen import QuizScreen
+from app.views.quizscreen.quizscreen import RegisterScreen
 from app.views.pdfscreen.pdfscreen import PdfScreen
 from app.views.settingscreen.settingscreen import SettingScreen
 from app.views.videoscreen.videoscreen import VideoScreen
@@ -16,6 +17,7 @@ class MainApp(App):
         sm = ScreenManager()
         sm.add_widget(HomeScreen())
         sm.add_widget(SettingScreen())
+        sm.add_widget(RegisterScreen())
         sm.add_widget(QuizScreen())
         sm.add_widget(PdfScreen())
         sm.add_widget(VideoScreen())
