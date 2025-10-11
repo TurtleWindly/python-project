9f476589297aefcca7cbac53cbeddabbe34b4d18
MinhWin <quachquangminh2006@gmail.com>
2025-10-11 10:41:27 +0700

    complete basic register screen

--- quiz box/app/views/registerscreen/registerscreen.py
+++ quiz box/app/views/registerscreen/registerscreen.py
@@ -1,3 +1,4 @@
+from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 
@@ -11,10 +12,22 @@ class RegisterScreen(Screen):
 
     # prefer a clearer method name, but reg is fine
     def register_user(self):
-        name = self.name_text.text_input.text
-        unit = self.unit_text.text_input.text
+        app = App.get_running_app()
+        name: str = self.name_text.text_input.text
+        unit: str = self.unit_text.text_input.text
 
-        if name and unit:
-            user_data = {"name": name, "unit": unit, "score": 0}
+        if not name and not unit:
+            # TODO: Create a popup to inform the user
+            return
+
+        name = name.strip()
+        unit = unit.strip()
+
+        if name == "" and unit == "":
+            # TODO: Create a popup to inform the user
+            return
+
+        user_data = {"name": name, "unit": unit, "score": 0}
             # do something with user_data, e.g. save or switch screen
-        print(name, unit)
+        app.current_user = app.db.add_result(name, unit, 0)
+        self.manager.current = "quiz_screen"
