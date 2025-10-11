6814b03b0b6e6a1591372c4ec3f7d3a72a60f936
MinhWin <quachquangminh2006@gmail.com>
2025-10-11 16:06:22 +0700

    seperate User class and added resutl screen

--- quiz box/main.py
+++ quiz box/main.py
@@ -2,31 +2,30 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.app import App
 
 from app.views.homescreen.homescreen import HomeScreen
-from app.views.quizscreen.quizscreen import QuizScreen
 from app.views.registerscreen.registerscreen import RegisterScreen
+from app.views.quizscreen.quizscreen import QuizScreen
+from app.views.resultscreen.resultscreen import ResultScreen
 from app.views.pdfscreen.pdfscreen import PdfScreen
 from app.views.settingscreen.settingscreen import SettingScreen
 from app.views.videoscreen.videoscreen import VideoScreen
 from app.database import QuizDatabase
 
-class User:
-    def __init__(self, name: str, unit: str, score: int = 0):
-        self.name = name
-        self.unit = unit
-        self.score = score
+from app.user import User
+
 
 # Create the manage
 class MainApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.db = QuizDatabase()
-        self.current_user = None
+        self.current_user = User("", "")
 
     def build(self):
         sm = ScreenManager()
         sm.add_widget(HomeScreen())
         sm.add_widget(SettingScreen())
         sm.add_widget(RegisterScreen())
+        sm.add_widget(ResultScreen())
         sm.add_widget(QuizScreen())
         sm.add_widget(PdfScreen())
         sm.add_widget(VideoScreen())
