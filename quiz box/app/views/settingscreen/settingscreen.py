78b7a3b00f5b8adcdd3455f3fe3332ec1d836385
MinhWin <quachquangminh2006@gmail.com>
2025-10-18 19:09:22 +0700

    added settings view

--- quiz box/app/views/settingscreen/settingscreen.py
+++ quiz box/app/views/settingscreen/settingscreen.py
@@ -1,5 +1,22 @@
+from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
 
 
 class SettingScreen(Screen):
     name = "setting_screen"
+
+    max_question_text = ObjectProperty(None)
+    volume_text = ObjectProperty(None)
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        app = App.get_running_app()
+        self.max_question_text = app.settings.max_question
+        self.volume_text = app.settings.volume
+
+    def save(self):
+        app = App.get_running_app()
+        app.settings.max_question = self.max_question_text
+        app.settings.volume = self.volume_text
+        app.settings.save()
