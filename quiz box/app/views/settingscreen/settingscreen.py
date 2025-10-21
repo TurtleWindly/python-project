5e566ae2b4212ddf5e51b26ebd6dc4b44bf920ab
MinhWin <quachquangminh2006@gmail.com>
2025-10-21 09:07:51 +0700

    change setting screen with kivymd widget

--- quiz box/app/views/settingscreen/settingscreen.py
+++ quiz box/app/views/settingscreen/settingscreen.py
@@ -1,22 +1,22 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, NumericProperty
 
 
 class SettingScreen(MDScreen):
     name = "setting_screen"
 
     max_question_text = ObjectProperty(None)
-    volume_text = ObjectProperty(None)
+    volume = NumericProperty(None)
 
     def __init__(self, **kw):
         super().__init__(**kw)
         app = MDApp.get_running_app()
         self.max_question_text = app.settings.max_question
-        self.volume_text = app.settings.volume
+        self.volume = app.settings.volume
 
     def save(self):
         app = MDApp.get_running_app()
         app.settings.max_question = self.max_question_text
-        app.settings.volume = self.volume_text
+        app.settings.volume = self.volume
         app.settings.save()
