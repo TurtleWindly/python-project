a0788c12600f58a07bee6f2cf7ea317f5e4dc70e
MinhWin <quachquangminh2006@gmail.com>
2025-10-22 17:17:52 +0700

    make video screen viable p1

--- quiz box/app/views/videoscreen/videoscreen.py
+++ quiz box/app/views/videoscreen/videoscreen.py
@@ -3,22 +3,23 @@ import shutil
 import sys
 import subprocess
 from pathlib import Path
-from kivy.uix.image import Image
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics.texture import Texture
+
+from kivy.properties import ObjectProperty
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.list import MDList, OneLineListItem
 from kivy.uix.button import Button
-from kivy.uix.label import Label
+from kivymd.uix.label import MDLabel
 
 VIDEO_FOLDER = Path.cwd() / "res" / "videos"
 
 
 class VideoScreen(MDScreen):
     """Màn hình hiển thị danh sách các video"""
-
     name = "video_screen"
+    video_list = ObjectProperty(None)
 
     def on_enter(self, *args):
+        print("Entering Video Screen")
         self.refresh_list()
 
     def open_file(self, path):
@@ -32,19 +33,19 @@ class VideoScreen(MDScreen):
 
     def refresh_list(self):
         """Làm mới danh sách các file video hiển thị."""
-        files_box = self.ids.files_box
-        files_box.clear_widgets()
+        video_list = self.ids.video_list
+        video_list.clear_widgets()
 
         video_files = [f for f in os.listdir(VIDEO_FOLDER) if f.lower().endswith(".mp4")]
 
         if not video_files:
-            files_box.add_widget(Label(text="Không tìm thấy file video nào"))
+            video_list.add_widget(MDLabel(text="Không tìm thấy file video nào"))
         else:
             for f in video_files:
-                btn = Button(text=f, size_hint_y=None, height=50)
-                btn.bind(
+                list_item = OneLineListItem(text=f)
+                list_item.bind(
                     on_release=lambda inst, filename=f: self.open_file(
                         VIDEO_FOLDER / filename
                     )
                 )
-                files_box.add_widget(btn)
+                video_list.add_widget(list_item)
