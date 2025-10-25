1d4ccaf0a484fd6f5560c61811abd2fcee1f3f00
MinhWin <quachquangminh2006@gmail.com>
2025-10-25 10:39:45 +0700

    better result screen

--- quiz box/app/views/resultscreen/resultscreen.py
+++ quiz box/app/views/resultscreen/resultscreen.py
@@ -6,16 +6,12 @@ from app.user import User
 
 class ResultScreen(MDScreen):
     name = "result_screen"
-    name_label = ObjectProperty(None)
-    unit_label = ObjectProperty(None)
-    score_label = ObjectProperty(None)
+    score_label = ObjectProperty("Điểm của bạn: ")
 
     def on_enter(self, **kw):
         app = App.get_running_app()
 
-        self.name_label.text = app.current_user.name
-        self.unit_label.text = app.current_user.unit
-        self.score_label.text = str(app.current_user.score)
+        self.score_label.text += str(app.current_user.score) + "/" + str(app.settings.max_question)
 
         # Clear the score after displaying it
         app.current_user.score = 0
