43ad3bc77079f757b6249ccc4b1f861f934886b9
MinhWin <quachquangminh2006@gmail.com>
2025-10-31 21:02:46 +0700

    add new screen

--- quiz box/app/views/addquizscreen/addquizscreen.py
+++ quiz box/app/views/addquizscreen/addquizscreen.py
@@ -1,11 +1,15 @@
+import json
 from kivy.app import App
 from kivymd.uix.screen import MDScreen
 from kivy.properties import ObjectProperty
 
-# from app.user import User
+json_file = "questions.json"
+with open(json_file, "r", encoding="utf-8") as f:
+    questions_data: list[dict] = json.load(f)
 
 class AddQuizScreen(MDScreen):
     name = "add_quiz_screen"
+
 
     # def on_enter(self, **kw):
     #     app = App.get_running_app()
